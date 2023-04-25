fff16ed732b4ec42f3a952638b53f7cc0023cca8
yoursTrulyChandan <chandan.xt@outlook.com>
2023-04-25 16:37:40 +0530

    Sidebar

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -1,11 +1,28 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { BsTwitter } from "react-icons/bs";
 
 const SidebarLogo = () => {
-    const router = useRouter();
+  const router = useRouter();
 
   return (
-    <div>SidebarLogo</div>
-  )
-}
+    <div
+      onClick={() => router.push("/")}
+      className="
+    rounded-full
+    h-14
+    w-14
+    p-4
+    flex
+    items-center
+    justify-center
+    hover:bg-blue-300
+    cursor-pointer
+    transition
+    "
+    >
+      <BsTwitter size={28} color="white" />
+    </div>
+  );
+};
 
-export default SidebarLogo
+export default SidebarLogo;
