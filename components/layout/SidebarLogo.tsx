4093e3d25f7f426433e67cb852a48ab08d8eedf8
yoursTrulyChandan <chandan.xt@outlook.com>
2023-04-25 12:41:41 +0530

    Setup

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+const SidebarLogo = () => {
+    const router = useRouter();
+
+  return (
+    <div>SidebarLogo</div>
+  )
+}
+
+export default SidebarLogo
