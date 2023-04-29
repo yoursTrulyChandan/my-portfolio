f3631399e7a8150029a046bf8b0b5d2ca2b77eee
yoursTrulyChandan <chandan.xt@outlook.com>
2023-04-29 14:39:49 +0530

    Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Modal isOpen actionLabel="Submit" cancel="Cancel" title="Do you want to share your tweet"/>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   );
 }
